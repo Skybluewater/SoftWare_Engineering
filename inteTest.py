4236e999e5205c260c3e0fe8d2017f2383b770cf
Skybluewater <Skybluewater@users.noreply.github.com>
2020-01-18 22:10:19 +0800

    对集成测试用例进行了修改

--- inteTest.py
+++ inteTest.py
@@ -32,17 +32,39 @@ class uniontest(unittest.TestCase):
         ret = self.question.generate(1001, 7, 1, 1, 1, 1, 9)
         print("generate questions")
 
-    def test_calculator1(self):
-        k = self.solution.calculator("(6-3)/4")
-        self.assertEqual(k, 0.75)
+    def test_ques6(self):
+        ret = self.question.generate(10, 5, 0, 0, 0, 9)
+        print("generate questions")
+        print(ret)
 
-    def test_calculator2(self):
-        k = self.solution.calculator("7")
-        self.assertEqual(k, 7)
+    def test_ques7(self):
+        ret = self.question.generate(100, 1, 0, 0, 15)
+        print("generate questions")
 
-    def test_calculator3(self):
-        k = self.solution.calculator("3-8")
-        self.assertEqual(k, -5)
+    def test_ques8(self):
+        ret = self.question.generate(10, 5, 1, 0, 0, 9)
+        print("generate questions")
+        print(ret)
+
+    def test_ques9(self):
+        ret = self.question.generate(10, 5, 0, 1, 0, 9)
+        print("generate questions")
+        print(ret)
+
+    def test_ques10(self):
+        ret = self.question.generate(10, 5, 0, 0, 1, 9)
+        print("generate questions")
+        print(ret)
+
+    def test_ques11(self):
+        ret = self.question.generate(5, 8, 0, 0, 0, 15)
+        print("generate questions")
+        print(ret)
+
+    def test_ques12(self):
+        ret = self.question.generate(1000, 5, 1, 1, 1, 9)
+        print("generate questions")
+        print(ret)
 
 
 if __name__ == '__main__':
@@ -52,8 +74,12 @@ if __name__ == '__main__':
     suite.addTest(uniontest('test_ques3'))
     suite.addTest(uniontest('test_ques4'))
     suite.addTest(uniontest('test_ques5'))
-    suite.addTest(uniontest('test_calculator1'))
-    suite.addTest(uniontest('test_calculator2'))
-    suite.addTest(uniontest('test_calculator3'))
+    suite.addTest(uniontest('test_ques6'))
+    suite.addTest(uniontest('test_ques7'))
+    suite.addTest(uniontest('test_ques8'))
+    suite.addTest(uniontest('test_ques9'))
+    suite.addTest(uniontest('test_ques10'))
+    suite.addTest(uniontest('test_ques11'))
+    suite.addTest(uniontest('test_ques12'))
     runner = unittest.TextTestRunner()
     runner.run(suite)
